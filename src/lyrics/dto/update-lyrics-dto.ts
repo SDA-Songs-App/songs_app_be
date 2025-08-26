57fbd2973f2d25341846532fb93f02722287bd33
Michael-enin <sys.dev.mike@gmail.com>
2025-08-26 23:03:30 +0300

    soft delete, dto and api implementation

--- src/lyrics/dto/update-lyrics-dto.ts
+++ src/lyrics/dto/update-lyrics-dto.ts
@@ -1,3 +1,17 @@
+import { CATEGORIES, LANGUAGES } from "@prisma/client";
+import {IsEnum, IsInt, IsOptional, IsString} from "class-validator";
+
 export class UpdateLyricsDto{
-    
+    @IsOptional() @IsInt()
+    contentId?:number
+    @IsOptional() @IsInt()
+    albumId?:number
+    @IsOptional() @IsInt()
+    artistId?:number
+    @IsOptional() @IsString()
+    audioFileUrl?:string
+    @IsEnum(LANGUAGES)
+    language:LANGUAGES
+    @IsEnum(CATEGORIES)
+    category:CATEGORIES
 }
