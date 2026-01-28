a257a0af14e74245630a1a4441e81e81cf99ac9c
Michael-enin <sys.dev.mike@gmail.com>
2026-01-28 22:46:40 +0300

    removed client

--- client/src/api/songsApi.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-const API_URL = "http://localhost:3000/";
-export const getSongs = ()=>axios.get(API_URL/lyrics);
-export const getSongById = (id) =>axios.get(`${API_URL}/lyrics/${id}`)
-export const addSong = (songData) => axios.post(API_URL/lyrics, songData)
-export const updateSong = (id, songData) => axios.put(`${API_URL}/lyrics/${id}`, songData)
-export const deleteSong = (id) =>axios.get(`${API_URL}/lyrics/${id}`)
-
